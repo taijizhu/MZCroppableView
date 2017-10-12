60dff58b2ac0ddb674b8a9eda1efffb6c649c1da
taijizhu <taijizhu@users.noreply.github.com>
2017-10-11 20:27:48 -0500

    Update MZCroppableView.m

--- MZCroppableView/Classes/MZCroppableView.m
+++ MZCroppableView/Classes/MZCroppableView.m
@@ -97,34 +97,6 @@
 }
 
 
-- (UIImage *)croppedImageInRect:(CGRect)rect
-{
-    double (^rad)(double) = ^(double deg) {
-        return deg / 180.0 * M_PI;
-    };
-    
-    CGAffineTransform rectTransform;
-    switch (self.imageOrientation) {
-        case UIImageOrientationLeft:
-            rectTransform = CGAffineTransformTranslate(CGAffineTransformMakeRotation(rad(90)), 0, -self.size.height);
-            break;
-        case UIImageOrientationRight:
-            rectTransform = CGAffineTransformTranslate(CGAffineTransformMakeRotation(rad(-90)), -self.size.width, 0);
-            break;
-        case UIImageOrientationDown:
-            rectTransform = CGAffineTransformTranslate(CGAffineTransformMakeRotation(rad(-180)), -self.size.width, -self.size.height);
-            break;
-        default:
-            rectTransform = CGAffineTransformIdentity;
-    };
-    rectTransform = CGAffineTransformScale(rectTransform, self.scale, self.scale);
-    
-    CGImageRef imageRef = CGImageCreateWithImageInRect([self CGImage], CGRectApplyAffineTransform(rect, rectTransform));
-    UIImage *result = [UIImage imageWithCGImage:imageRef scale:self.scale orientation:self.imageOrientation];
-    CGImageRelease(imageRef);
-    
-    return result;
-}
 
 - (CGRect) cropRect:(UIImageView*)image
 {
